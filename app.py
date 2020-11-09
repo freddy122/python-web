9180efdd63479a2a1aa095eaadcad5c052ea68d2
frederic.razafimampiandra <f.razafimampiandra@insyniumgroup.com>
2020-11-09 04:34:06 -0400

    sef

--- app.py
+++ app.py
@@ -82,5 +82,4 @@ def create_product():
     result = product_schema.dump(product.create())
     return make_response(jsonify({"product": result}),200)
 if __name__ == "__main__":
-
     app.run(debug=True)
